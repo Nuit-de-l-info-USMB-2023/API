d26e51086ee1b9aeba6d963d5b72e76384cbe92f
Kylian BACHELET <kykydesavoie@hotmail.com>
2023-12-08 01:40:14 +0100

    feat Possibilité création d'une publication

---
+++ src/main/java/com/starter_kits_usmb/back_java_spring_boot/post/Green.java
@@ -0,0 +1,33 @@
+package com.starter_kits_usmb.back_java_spring_boot.post;
+
+
+import com.starter_kits_usmb.back_java_spring_boot.user.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "posts")
+@Data
+public class Green {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "description")
+    @NotBlank
+    private String description;
+
+    @Column(name = "date")
+    @NotBlank
+    private Date date;
+
+    @Column(name = "photo")
+    private String image;
+}
---
+++ src/main/java/com/starter_kits_usmb/back_java_spring_boot/post/GreenRepository.java
@@ -0,0 +1,6 @@
+package com.starter_kits_usmb.back_java_spring_boot.post;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GreenRepository extends JpaRepository<Green, Long> {
+}
